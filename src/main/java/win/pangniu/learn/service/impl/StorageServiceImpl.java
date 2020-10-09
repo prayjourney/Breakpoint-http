74856f9e2b99944e72fa3c656b673edcb626da4e
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-10-09 15:05:58 +0800

    🎣🎣🎣注释

--- src/main/java/win/pangniu/learn/service/impl/StorageServiceImpl.java
+++ src/main/java/win/pangniu/learn/service/impl/StorageServiceImpl.java
@@ -36,7 +36,7 @@ public class StorageServiceImpl implements StorageService {
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
-    //这个必须与前端设定的值一致
+    // 这个必须与前端设定的值一致
     @Value("${breakpoint.upload.chunkSize}")
     private long CHUNK_SIZE;
 
@@ -81,9 +81,9 @@ public class StorageServiceImpl implements StorageService {
 
         RandomAccessFile accessTmpFile = new RandomAccessFile(tmpFile, "rw");
         long offset = CHUNK_SIZE * param.getChunk();
-        //定位到该分片的偏移量
+        // 定位到该分片的偏移量
         accessTmpFile.seek(offset);
-        //写入该分片数据
+        // 写入该分片数据
         accessTmpFile.write(param.getFile().getBytes());
         // 释放
         accessTmpFile.close();
@@ -109,7 +109,7 @@ public class StorageServiceImpl implements StorageService {
         RandomAccessFile tempRaf = new RandomAccessFile(tmpFile, "rw");
         FileChannel fileChannel = tempRaf.getChannel();
 
-        //写入该分片数据
+        // 写入该分片数据
         long offset = CHUNK_SIZE * param.getChunk();
         byte[] fileData = param.getFile().getBytes();
         MappedByteBuffer mappedByteBuffer = fileChannel.map(FileChannel.MapMode.READ_WRITE, offset, fileData.length);
@@ -137,17 +137,17 @@ public class StorageServiceImpl implements StorageService {
         String fileName = param.getName();
         File confFile = new File(uploadDirPath, fileName + ".conf");
         RandomAccessFile accessConfFile = new RandomAccessFile(confFile, "rw");
-        //把该分段标记为 true 表示完成
+        // 把该分段标记为 true 表示完成
         System.out.println("set part " + param.getChunk() + " complete");
         accessConfFile.setLength(param.getChunks());
         accessConfFile.seek(param.getChunk());
         accessConfFile.write(Byte.MAX_VALUE);
 
-        //completeList 检查是否全部完成,如果数组里是否全部都是(全部分片都成功上传)
+        // completeList 检查是否全部完成,如果数组里是否全部都是(全部分片都成功上传)
         byte[] completeList = FileUtils.readFileToByteArray(confFile);
         byte isComplete = Byte.MAX_VALUE;
         for (int i = 0; i < completeList.length && isComplete == Byte.MAX_VALUE; i++) {
-            //与运算, 如果有部分没有完成则 isComplete 不是 Byte.MAX_VALUE
+            // 与运算, 如果有部分没有完成则 isComplete 不是 Byte.MAX_VALUE
             isComplete = (byte) (isComplete & completeList[i]);
             System.out.println("check part " + i + " complete?:" + completeList[i]);
         }
@@ -176,14 +176,14 @@ public class StorageServiceImpl implements StorageService {
      * @return
      */
     public boolean renameFile(File toBeRenamed, String toFileNewName) {
-        //检查要重命名的文件是否存在，是否是文件
+        // 检查要重命名的文件是否存在，是否是文件
         if (!toBeRenamed.exists() || toBeRenamed.isDirectory()) {
             logger.info("File does not exist: " + toBeRenamed.getName());
             return false;
         }
         String p = toBeRenamed.getParent();
         File newFile = new File(p + File.separatorChar + toFileNewName);
-        //修改文件名
+        // 修改文件名
         return toBeRenamed.renameTo(newFile);
     }
 
