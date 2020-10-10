87e98176a27bf7535720a63e26508226597d96ce
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-10-10 16:36:09 +0800

    🎏🎏🎏调整

--- src/main/java/win/pangniu/learn/param/MultipartFileParam.java
+++ src/main/java/win/pangniu/learn/param/MultipartFileParam.java
@@ -8,22 +8,21 @@ import org.springframework.web.multipart.MultipartFile;
  * version 1.0
  */
 public class MultipartFileParam {
-
     // 用户id
     private String uid;
-    //任务ID
+    // 任务ID
     private String id;
-    //总分片数量
+    // 总分片数量
     private int chunks;
-    //当前为第几块分片
+    // 当前为第几块分片
     private int chunk;
-    //当前分片大小
+    // 当前分片大小
     private long size = 0L;
-    //文件名
+    // 文件名
     private String name;
-    //分片对象
+    // 分片对象
     private MultipartFile file;
-    // MD5
+    // MD5值
     private String md5;
 
     public String getUid() {
