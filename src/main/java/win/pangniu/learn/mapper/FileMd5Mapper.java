792e31c5ea72e8f5c64a71d79e85db2936b4d42e
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-10-12 16:48:38 +0800

    🎋🎋🎋Mapper文件

---
+++ src/main/java/win/pangniu/learn/mapper/FileMd5Mapper.java
@@ -0,0 +1,14 @@
+package win.pangniu.learn.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+import win.pangniu.learn.pojo.FileMd5;
+
+/**
+ * @Author renjiaxin
+ * @Date 2020/10/12
+ * @Description
+ */
+@Repository
+public interface FileMd5Mapper extends BaseMapper<FileMd5> {
+}
