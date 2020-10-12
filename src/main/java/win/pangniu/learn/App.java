d0eda8714a7ce14b0d6079dc25150311668c6d3f
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-10-12 16:45:34 +0800

    🎋🎋🎋mapper扫描开启

--- src/main/java/win/pangniu/learn/App.java
+++ src/main/java/win/pangniu/learn/App.java
@@ -1,5 +1,6 @@
 package win.pangniu.learn;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * version 1.0
  */
 @SpringBootApplication
+@MapperScan("win.pangniu.learn.mapper")
 public class App {
 
     public static void main(String[] args) {
