b0eee6da452df22de345b0aed6f2e3178921ae64
lin_zhaoxi <lin_zhaoxi@sohu.com>
2021-01-29 18:24:50 +0800

    😑😑😑分片上传包装

--- src/main/java/win/pangniu/learn/param/MultipartFileParam.java
+++ src/main/java/win/pangniu/learn/param/MultipartFileParam.java
@@ -1,12 +1,22 @@
 package win.pangniu.learn.param;
 
-
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
- * Created by wenwen on 2017/4/16.
- * version 1.0
+ * 分片上传包装
  */
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
 public class MultipartFileParam {
     // 用户id
     private String uid;
@@ -25,81 +35,4 @@ public class MultipartFileParam {
     // MD5值
     private String md5;
 
-    public String getUid() {
-        return uid;
-    }
-
-    public void setUid(String uid) {
-        this.uid = uid;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public int getChunks() {
-        return chunks;
-    }
-
-    public void setChunks(int chunks) {
-        this.chunks = chunks;
-    }
-
-    public int getChunk() {
-        return chunk;
-    }
-
-    public void setChunk(int chunk) {
-        this.chunk = chunk;
-    }
-
-    public long getSize() {
-        return size;
-    }
-
-    public void setSize(long size) {
-        this.size = size;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public MultipartFile getFile() {
-        return file;
-    }
-
-    public void setFile(MultipartFile file) {
-        this.file = file;
-    }
-
-    public String getMd5() {
-        return md5;
-    }
-
-    public void setMd5(String md5) {
-        this.md5 = md5;
-    }
-
-    @Override
-    public String toString() {
-        return "MultipartFileParam{" +
-                "uid='" + uid + '\'' +
-                ", id='" + id + '\'' +
-                ", chunks=" + chunks +
-                ", chunk=" + chunk +
-                ", size=" + size +
-                ", name='" + name + '\'' +
-                ", file=" + file +
-                ", md5='" + md5 + '\'' +
-                '}';
-    }
 }
